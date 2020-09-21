161b6d808bbf62ff86580f9051c68c9a9cfa2288
CAMANEM <camanem10@hotmail.com>
2020-09-20 23:00:58 -0600

    Actualización de interfaz grafica

--- src/Chat/Interfaz.java
+++ src/Chat/Interfaz.java
@@ -3,50 +3,63 @@ package Chat;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 
 public class Interfaz {
 
-    JFrame ventana_chat;
+    String username;
+    String destinatario; //cambiar a int
+    JFrame ventana;
     JButton btn_enviar;
     JTextField txt_mensaje;
-    JTextArea area_chat;
-    JPanel contenedor_areachat;
+    JTextArea vista_chat;
+    JPanel contenedor_vista_chat;
     JPanel contenedor_btntxt;
     JScrollPane scroll;
-    Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
 
     public static void main(String[] args) {
         new Interfaz();
     }
 
     public Interfaz(){
+        username = JOptionPane.showInputDialog("Digite su nombre de usuario: ");
+        destinatario = JOptionPane.showInputDialog("Digite el puerto de su destinatario: ");
         hacerInterfaz();
     }
 
-    public void hacerInterfaz(){
-        ventana_chat = new JFrame("Chat");
+    public void hacerInterfaz() {
+        ventana = new JFrame("Chat");
         btn_enviar = new JButton("Enviar");
         txt_mensaje = new JTextField(4);
-        area_chat = new JTextArea(27, 3);
-        //area_chat.setEditable(false);
-        scroll = new JScrollPane(area_chat);
-        contenedor_areachat = new JPanel();
-        contenedor_areachat.setLayout(new GridLayout(1,1));
-        contenedor_areachat.add(scroll);
+        vista_chat = new JTextArea(27, 3);
+        vista_chat.setEditable(false);
+        scroll = new JScrollPane(vista_chat);
+        contenedor_vista_chat = new JPanel();
+        contenedor_vista_chat.setLayout(new GridLayout(1, 1));
+        contenedor_vista_chat.add(scroll);
         contenedor_btntxt = new JPanel();
-        contenedor_btntxt.setLayout(new GridLayout(1,2));
+        contenedor_btntxt.setLayout(new GridLayout(1, 2));
         contenedor_btntxt.add(txt_mensaje);
         contenedor_btntxt.add(btn_enviar);
-        ventana_chat.setLayout(new BorderLayout());
-        ventana_chat.add(contenedor_areachat, BorderLayout.NORTH);
-        ventana_chat.add(contenedor_btntxt, BorderLayout.SOUTH);
-        ventana_chat.setSize(500, 500);
-        ventana_chat.setLocationRelativeTo(null);
-        ventana_chat.setVisible(true);
-        ventana_chat.setResizable(false);
-        ventana_chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLayout(new BorderLayout());
+        ventana.add(contenedor_vista_chat, BorderLayout.NORTH);
+        ventana.add(contenedor_btntxt, BorderLayout.SOUTH);
+        ventana.setSize(500, 500);
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+        ventana.setResizable(false);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        btn_enviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                enviar_msg();
+            }
+        });
+    }
+    public void enviar_msg(){
+        System.out.println(this.txt_mensaje.getText());
+        vista_chat.append("Tú: " + this.txt_mensaje.getText() + "\n");
     }
 
 }
